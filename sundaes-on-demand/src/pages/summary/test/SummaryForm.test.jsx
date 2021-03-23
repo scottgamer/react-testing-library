f5af1649bf1367ff8696b967e2effa5db1b27c66
rmunoz <richosojason@msn.com>
2021-03-23 16:50:41 -0500

    added checkbox test conditions

--- sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
+++ sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -13,4 +13,21 @@ describe("Summary Form", () => {
     });
     expect(confirmButton).toBeDisabled();
   });
+
+  test("checkbox disables button on first click and disables on second click", () => {
+    render(<SummaryForm />);
+
+    const checkbox = screen.getByRole("checkbox", {
+      name: /terms and conditions/i,
+    });
+    const confirmButton = screen.getByRole("button", {
+      name: /confirm order/i,
+    });
+
+    fireEvent.click(checkbox);
+    expect(confirmButton).toBeEnabled();
+
+    fireEvent.click(checkbox);
+    expect(confirmButton).toBeDisabled();
+  });
 });
