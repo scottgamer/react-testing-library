172cd5dce919f7f46d9c5d281ed366d77a7f7c85
rmunoz <richosojason@msn.com>
2021-04-04 23:01:13 -0500

    added toppings test

--- sundaes-on-demand/src/pages/entry/tests/Options.test.jsx
+++ sundaes-on-demand/src/pages/entry/tests/Options.test.jsx
@@ -13,3 +13,19 @@ test("displays image for each scoop", async () => {
   const altText = scoopImages.map((element) => element.alt);
   expect(altText).toEqual(["chocolate scoop", "vanilla scoop"]);
 });
+
+test("displays image for each toppings option from server", async () => {
+  // mock service worker will return 3 toppings from server
+  render(<Options optionType="toppings" />);
+  // find images, expect 3 based on what msw returns
+  const images = await screen.findAllByRole("img", { name: /topping$/i });
+  expect(images).toHaveLength(3);
+
+  // check actual alt text for the images
+  const imageTitles = images.map((img) => img.alt);
+  expect(imageTitles).toStrictEqual([
+    "Cherries topping",
+    "M&Ms topping",
+    "Hot fudge topping",
+  ]);
+});
