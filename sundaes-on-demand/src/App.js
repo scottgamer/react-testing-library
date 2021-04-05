6ec7d0d375b0eabcfba0e218a58ee5d9ba046b36
rmunoz <richosojason@msn.com>
2021-04-04 23:00:44 -0500

    added options component to main component

--- sundaes-on-demand/src/App.js
+++ sundaes-on-demand/src/App.js
@@ -1,9 +1,11 @@
 import SummaryForm from "./pages/summary/SummaryForm";
+import Options from "./pages/entry/Options.jsx";
 
 function App() {
   return (
     <>
       <SummaryForm />
+      <Options />
       <p>Learn React</p>
     </>
   );
