8dea2c5423b2c02bc8126b1d94780c2c56918156
rmunoz <richosojason@msn.com>
2021-04-05 18:54:22 -0500

    added alert banner to options

--- sundaes-on-demand/src/pages/entry/Options.jsx
+++ sundaes-on-demand/src/pages/entry/Options.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 
 import axios from "axios";
-import Row from "react-bootstrap/Row"
+import Row from "react-bootstrap/Row";
 import Scoop from "./Scoop";
 import Topping from "./Topping";
+import AlertBanner from "../common/AlertBanner";
 
 const Options = ({ optionType }) => {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -14,12 +16,16 @@ const Options = ({ optionType }) => {
         const response = await axios.get(`http://localhost:3030/${optionType}`);
         setItems(response.data);
       } catch (error) {
-        console.error(error);
+        setError(true);
       }
     };
 
     fetchData();
   }, [optionType]);
+
+  if (error) {
+    return <AlertBanner />;
+  }
 
   const ItemComponent = optionType === "scoops" ? Scoop : Topping;
 
