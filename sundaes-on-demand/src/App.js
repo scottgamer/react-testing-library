a7d71d6cd0d471ac4eea2d2ac4c81986554140c4
rmunoz <richosojason@msn.com>
2021-04-14 22:16:11 -0500

    added order provider

--- sundaes-on-demand/src/App.js
+++ sundaes-on-demand/src/App.js
@@ -1,12 +1,22 @@
 import SummaryForm from "./pages/summary/SummaryForm";
 import Options from "./pages/entry/Options.jsx";
 
+import Container from "react-bootstrap/Container";
+import OrderEntry from "./pages/entry/OrderEntry";
+
+import { OrderDetailsProvider } from "./context/OrderDetails";
+
 function App() {
   return (
     <>
-      <SummaryForm />
-      <Options />
-      <p>Learn React</p>
+      <Container>
+        <OrderDetailsProvider>
+          <OrderEntry />
+          <SummaryForm />
+          <Options />
+          <p>Learn React</p>
+        </OrderDetailsProvider>
+      </Container>
     </>
   );
 }
