1decaccbed7666ebc510d5ee8f7019ced9343c51
rmunoz <richosojason@msn.com>
2021-04-14 22:17:09 -0500

    added order detail context to options component

--- sundaes-on-demand/src/pages/entry/Options.jsx
+++ sundaes-on-demand/src/pages/entry/Options.jsx
@@ -5,10 +5,13 @@ import Row from "react-bootstrap/Row";
 import Scoop from "./Scoop";
 import Topping from "./Topping";
 import AlertBanner from "../common/AlertBanner";
+import { pricePerItem } from "../../constants";
+import { useOrderDetails } from "../../context/OrderDetails";
 
 const Options = ({ optionType }) => {
   const [items, setItems] = useState([]);
   const [error, setError] = useState(false);
+  const [orderDetails, updateItemCount] = useOrderDetails();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -28,16 +31,27 @@ const Options = ({ optionType }) => {
   }
 
   const ItemComponent = optionType === "scoops" ? Scoop : Topping;
+  const title = optionType[0].toUpperCase() + optionType.slice(1);
 
   const optionItems = items.map((item) => (
     <ItemComponent
       key={item.name}
       name={item.name}
       imagePath={item.imagePath}
+      updateItemCount={(itemName, newItemCount) =>
+        updateItemCount(itemName, newItemCount, optionType)
+      }
     />
   ));
 
-  return <Row>{optionItems}</Row>;
+  return (
+    <>
+      <h2>{title}</h2>
+      <p>{pricePerItem[optionType]} each</p>
+      <p>{title} total: {orderDetails.totals[optionType]}</p>
+      <Row>{optionItems}</Row>
+    </>
+  );
 };
 
 export default Options;
