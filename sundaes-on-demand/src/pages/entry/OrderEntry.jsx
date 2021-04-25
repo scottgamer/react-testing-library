450d81ff0f3e061066751d9a1c7e0f6f0a644b64
rmunoz <richosojason@msn.com>
2021-04-25 13:00:03 -0500

    added order detials for grand total

--- sundaes-on-demand/src/pages/entry/OrderEntry.jsx
+++ sundaes-on-demand/src/pages/entry/OrderEntry.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { useOrderDetails } from "../../context/OrderDetails";
 
 import Options from "./Options";
 
 const OrderEntry = () => {
+  const [orderDetails] = useOrderDetails();
+
   return (
     <>
+      <h1>Design Your Sundae!</h1>
       <Options optionType="scoops" />
       <Options optionType="toppings" />
+      <h2>Grand total: {orderDetails.totals.grandTotal}</h2>
     </>
   );
 };
